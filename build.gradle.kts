b9b10adc965546cc7d0df0c9569c78e850cb6697
Kagurazaka Mikoto <58372301+M1k0t0@users.noreply.github.com>
2021-10-12 11:45:10 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    kotlin("jvm") version "1.5.30"
-    kotlin("plugin.serialization") version "1.5.30"
-    kotlin("kapt") version "1.5.30"
+    kotlin("jvm") version "1.6.0-M1"
+    kotlin("plugin.serialization") version "1.6.0-M1"
+    kotlin("kapt") version "1.6.0-M1"
     id("com.github.johnrengelman.shadow") version "6.0.0"
     id("com.github.gmazzo.buildconfig") version "2.0.2"
 }
