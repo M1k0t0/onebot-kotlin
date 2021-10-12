985102cbcece806d8e308049a9ec701d4671460f
Kagurazaka Mikoto <58372301+M1k0t0@users.noreply.github.com>
2021-10-12 10:26:58 +0800

    Update Versions.kt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,10 +2,10 @@ import java.io.File
 import java.util.concurrent.TimeUnit
 
 object Versions {
-    const val projectVersion = "0.3.4"
+    const val projectVersion = "0.3.6"
 
-    const val miraiCoreVersion = "2.4.0"
-    const val miraiConsoleVersion = "2.4.0"
+    const val miraiCoreVersion = "2.7.1"
+    const val miraiConsoleVersion = "2.7.1"
 
     const val ktorVersion = "1.5.0"
     const val kotlinVersion = "1.4.21"
